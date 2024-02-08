8568e7b9a1443ae17046c94a086f7f48570397c1
Gulnara1406 <nabgulnara@mail.ru>
2024-02-08 23:41:04 +0300

    Devise settings

--- app/controllers/registrarions_controller.rb
+++ app/controllers/registrarions_controller.rb
@@ -1,62 +1,52 @@
-class UsersController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
 
   before_action :edit, only: [:update]
-
+  
   def new
     session[:current_time] = Time.now
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
-
+  
     if @user.save
       redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
     else
       render :new
     end
-    #if @user.save
-     # redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
-    #elsif !@user.password_digest?(user_params[:password])
-     # flash.now[:alert] = 'Вы неправильно заполнили поля регистрации'
-
-      #render :new
-    #else
-     # flash.now[:alert] = 'Что-то пошло не так. Попробуйте ещё раз.'
-
-      #render :new
-    #end
   end
-
-  def edit 
+  
+  def edit
     @user = User.find(params[:id])
   end
-
+  
   def update
     if @user.update(user_params)
       redirect_to root_path, notice: 'Данные пользователя обновлены'
     else
       flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
-
+  
       render :edit
     end
   end
-
-  def destroy 
+  
+  def destroy
     @user = User.find(params[:id])
     @user.destroy
-
+  
     session.delete(:user_id)
-
+  
     redirect_to root_path, notice: 'Пользователь удален'
   end
-
+  
   private
-
+  
   def user_params
     params.require(:user).permit(
-      :name, :nickname, :email, :password, :password_confirmation)
+    :name, :nickname, :email, :password, :password_confirmation)
   end
 end
+  
